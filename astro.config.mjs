9a34c553af12d613e938e2c36233a5ed50bc80cf
palominodev <jeremypalomino.dev@gmail.com>
2024-03-20 17:00:29 -0500

    prueba 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://palominodev.github.io',
-  base: '/'
+  site: 'https://palominodev.github.io/palominodev',
 });
